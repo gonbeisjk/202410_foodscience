260406e5556a2b39dc4f6a1cba8303506f34e1a0
taro shinjyuku <tarosjk@gmail.com>
2025-02-04 14:01:29 +0900

    固定ページのループを組み込み

--- wp-content/themes/foodscience/page.php
+++ wp-content/themes/foodscience/page.php
@@ -1,27 +1,25 @@
 <?php get_header(); ?>
 
-<main>
-  <section class="section">
-    <div class="section_inner">
-      <div class="section_header">
-        <h2 class="heading heading-primary"><span>コンセプト</span>CONCEPT</h2>
-      </div>
+<?php if (have_posts()): ?>
+  <?php while (have_posts()): the_post(); ?>
+    <main>
+      <section class="section">
+        <div class="section_inner">
+          <div class="section_header">
+            <h2 class="heading heading-primary"><span><?php the_title(); ?></span><?= $post->post_name; ?></h2>
+          </div>
 
-      <div class="section_body">
-        <div class="content">
+          <div class="section_body">
+            <div class="content">
 
-          <img src="assets/img/concept/banner_img@2x.jpg" alt="">
+              <?php the_content(); ?>
 
-          <p>FOOD SCIENCEでご提供するメキシコ料理は、当店の店主が修行したローカルフードを中心にした、ご家族でも楽しめる美味しいメキシカンです。</p>
-          <p>
-            スパイシーでヘルシーな本場の味をお楽しみ下さい。<br>
-            皆様のご来店お待ちしております。
-          </p>
-
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </section>
-</main>
+      </section>
+    </main>
+  <?php endwhile; ?>
+<?php endif; ?>
 
 <?php get_footer(); ?>
