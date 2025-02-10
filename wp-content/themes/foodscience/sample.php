7fa146dfb8a4c256f8f963384944680e3a226619
taro shinjyuku <tarosjk@gmail.com>
2025-02-10 09:57:20 +0900

    カスタムテンプレートを追加

---
+++ wp-content/themes/foodscience/sample.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template name: サンプルテンプレート
+Template Post Type: post, page, event
+*/
+// Template Post Typeはこのテンプレートが有効になる投稿タイプを指定する（post: 投稿、page: 固定ページ, event: 「イベント」カスタム投稿タイプ）。省略した場合は page のみ。
+?>
+<?php get_header(); ?>
+
+<main>
+  <p>サンプルカスタムテンプレート</p>
+</main>
+
+<?php get_footer(); ?>
