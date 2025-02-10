65018fbb40fb65f9987000cfbe3a749d7698c1e9
taro shinjyuku <tarosjk@gmail.com>
2025-02-10 11:16:21 +0900

    パスワード保護フォームをカスタマイズ

--- wp-content/themes/foodscience/functions.php
+++ wp-content/themes/foodscience/functions.php
@@ -75,3 +75,41 @@ function my_pre_get_posts($query)
     return;
   }
 }
+
+
+/**
+ * タイトルの「保護中」の文字を削除する
+ */
+add_filter('protected_title_format', 'my_protected_title');
+// $titleには「保護中: %s」という文字が代入されている
+function my_protected_title($title)
+{
+  return '%s';
+}
+
+
+/**
+ * パスワード保護フォームをカスタマイズする
+ */
+add_filter('the_password_form', 'my_password_form');
+function my_password_form()
+{
+  // 自動整形機能をOFFにする(brタグがインライン要素の後に自動追加されるのを防ぐ)
+  remove_filter('the_content', 'wpautop');
+
+  // ログインページのURLを取得 http://****/wp-login.php
+  $wp_login_url = wp_login_url();
+
+  // パスワード入力フォームを作成
+  // 【重要】formのaction属性、入力欄のname属性はオリジナルに合わせる
+  // 文字列の書き方にHeredoc（複数行、変数に対応した書き方）を使用。ダブルクォーテーションと同じ働きをする。
+  $html = <<<HTML
+  <p>パスワードを入力してください。</p>
+  <form class="post-password-form" action="{$wp_login_url}?action=postpass" method="post">
+    <input type="password" name="post_password">
+    <input type="submit" value="送信" name="送信">
+  </form>
+HTML;
+
+  return $html;
+}
