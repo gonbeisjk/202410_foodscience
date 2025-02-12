1cf87cb491e12e4a9ca28ce522d0ef10e2bff0b7
taro shinjyuku <tarosjk@gmail.com>
2025-02-12 14:52:26 +0900

    メニュー一覧ページの作成。メニューごとの繰り返し部分を実装（※商品のループは未済）

--- wp-content/themes/foodscience/archive-food.php
+++ wp-content/themes/foodscience/archive-food.php
@@ -7,67 +7,33 @@
         <h2 class="heading heading-primary"><span>フード紹介</span>FOOD</h2>
       </div>
 
-      <section class="section_body">
-        <h3 class="heading heading-secondary">お食事<span>MEAL</span></h3>
-        <ul class="foodList">
-
-          <?php if (have_posts()): ?>
-            <?php while (have_posts()): the_post(); ?>
-              <li class="foodList_item">
-                <?php get_template_part('template-parts/loop-food'); ?>
-              </li>
-            <?php endwhile; ?>
-          <?php endif; ?>
-
-        </ul>
-      </section>
-
-      <section class="section_body">
-        <h3 class="heading heading-secondary">ドリンク<span>DRINK</span></h3>
-        <ul class="foodList">
-          <li class="foodList_item">
-            <div class="foodCard">
-              <a href="#">
-                <div class="foodCard_pic">
-                  <img src="assets/img/food/drink_img01@2x.png" alt="">
-                </div>
-                <div class="foodCard_body">
-                  <h4 class="foodCard_title">ビール</h4>
-                  <p class="foodCard_price">¥700</p>
-                </div>
+      <?php
+      $menu_terms = get_terms(['taxonomy' => 'menu']);
+      ?>
+      <?php if (!empty($menu_terms)): ?>
+        <?php foreach ($menu_terms as $menu): ?>
+          <section class="section_body">
+            <h3 class="heading heading-secondary">
+              <a href="<?= get_term_link($menu); ?>">
+                <?= $menu->name ?>
               </a>
-            </div>
-          </li>
+              <span><?= strtoupper($menu->slug); ?></span>
+            </h3>
+            <ul class="foodList">
 
-          <li class="foodList_item">
-            <div class="foodCard">
-              <a href="#">
-                <div class="foodCard_pic">
-                  <img src="assets/img/food/drink_img02@2x.png" alt="">
-                </div>
-                <div class="foodCard_body">
-                  <h4 class="foodCard_title">アイスコーヒー</h4>
-                  <p class="foodCard_price">¥600</p>
-                </div>
-              </a>
-            </div>
-          </li>
+              <?php if (have_posts()): ?>
+                <?php while (have_posts()): the_post(); ?>
+                  <li class="foodList_item">
+                    <?php get_template_part('template-parts/loop-food'); ?>
+                  </li>
+                <?php endwhile; ?>
+              <?php endif; ?>
 
-          <li class="foodList_item">
-            <div class="foodCard">
-              <a href="#">
-                <div class="foodCard_pic">
-                  <img src="assets/img/food/drink_img03@2x.png" alt="">
-                </div>
-                <div class="foodCard_body">
-                  <h4 class="foodCard_title">コーヒー</h4>
-                  <p class="foodCard_price">¥500</p>
-                </div>
-              </a>
-            </div>
-          </li>
-        </ul>
-      </section>
+            </ul>
+          </section>
+        <?php endforeach; ?>
+      <?php endif; ?>
+
     </div>
   </section>
 </main>
