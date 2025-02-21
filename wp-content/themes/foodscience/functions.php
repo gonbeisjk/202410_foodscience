3069a581c1ae74566a0203d88acb0d823e255a48
taro shinjyuku <tarosjk@gmail.com>
2025-02-21 09:12:32 +0900

    フード一覧のカードにカスタムフィールドの値を出力

--- wp-content/themes/foodscience/functions.php
+++ wp-content/themes/foodscience/functions.php
@@ -4,6 +4,8 @@ add_theme_support('title-tag');
 // -----アイキャッチ画像を使用可能にする-----
 add_theme_support('post-thumbnails');
 
+add_theme_support('automatic-feed-links');
+
 // -----title内のセパレータを「|」に変更する-----
 
 // title内のセパレータをen-dashにしている、
